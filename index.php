e18a2bcfc14b3ca6a83f3636498915562312012f
Milosz Dura <milosz.md@wp.pl>
2017-05-23 19:44:31 +0100

    no need to echo the router contents

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Partham\Core\Router;
 
-echo Router::start(array_values(array_filter(explode('/', $_SERVER['REQUEST_URI']))), $_SERVER['REQUEST_METHOD']);
+Router::start(array_values(array_filter(explode('/', $_SERVER['REQUEST_URI']))), $_SERVER['REQUEST_METHOD']);
