becf72804763f87d4fd95d16716716179b6631ad
Milosz Dura <milosz.md@wp.pl>
2017-05-24 13:27:16 +0100

    moving request parsing higher so that we can see it earlier

--- core/Deploy.php
+++ core/Deploy.php
@@ -16,15 +16,15 @@ class Deploy
 
         $identifier = Helpers::GUID();
 
+        if (strpos($request, 'payload=') >= 0)
+            $request = str_replace('payload=', '', urldecode($request));
+
         $this->database->insert('deploys', ['log', 'request', 'status', 'startTime', 'identifier'], ['deploy started...', $request, 'started', date("Y-m-d H:i:s"), $identifier]);
 
         $output = shell_exec("cd /var/www/{$appName} && sudo bash deploy.sh 2>&1");
 
         if (is_null($output))
             return;
-
-        if (strpos($request, 'payload=') >= 0)
-            $request = str_replace('payload=', '', urldecode($request));
 
         $this->database->update('deploys', ['identifier' => $identifier], ['log' => $output, 'request' => $request, 'status' => 'success', 'endTime' => date("Y-m-d H:i:s")]);
     }
