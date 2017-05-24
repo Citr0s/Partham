f3b7ea011787d17ee36e9d450044a9e521b338c9
Milosz Dura <milosz.md@wp.pl>
2017-05-24 07:33:32 +0100

    adding more logging

--- core/Deploy.php
+++ core/Deploy.php
@@ -14,6 +14,8 @@ class Deploy
         if (!in_array($appName, $this->allowedApps))
             return;
 
+        $this->database->insert('deploys', ['log', 'request', 'status'], ['nothing yet', $request, 'before']);
+
         $output = shell_exec("cd /var/www/{$appName} && sudo bash deploy.sh 2>&1");
 
         $this->database->insert('deploys', ['log', 'request', 'status'], [$output, $request, 'maybe']);
