bd9ea168ca9301c0144d29e0d64bf4a96c748e2d
Milosz Dura <milosz.md@wp.pl>
2017-09-05 21:40:33 +0100

    poke to make sure the process box is being displayed

--- web/js/main.ts
+++ web/js/main.ts
@@ -132,6 +132,7 @@ function displayBuilds(data) {
 
     let secondsSinceStart = Math.floor((new Date().getTime()) / 1000 - parsedData[0].startTime);
     let minutesSinceStart = Math.floor(secondsSinceStart / 60);
+
     secondsSinceStart -= minutesSinceStart * 60;
 
     document.querySelector('.process-box .app-name').innerHTML = parsedData[0].appName;
