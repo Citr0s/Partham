dc4f25896e993474948e66f4e65579e44881fcab
Milosz Dura <milosz.md@wp.pl>
2017-09-06 13:02:41 +0100

    mapping app name through to the build process box

--- core/mappers/BuildModelMapper.php
+++ core/mappers/BuildModelMapper.php
@@ -1,13 +1,21 @@
 <?php namespace Partham\core\mappers;
 
+use Partham\core\repositories\AppRepository;
 use Partham\core\types\BuildModel;
 
 class BuildModelMapper
 {
-    public static function map($record)
+    private $appRepository;
+
+    public function __construct(AppRepository $appRepository)
+    {
+        $this->appRepository = $appRepository;
+    }
+
+    public function map($record)
     {
         $buildModel = new BuildModel();
-        $buildModel->appName = $record->app;
+        $buildModel->appName = $this->appRepository->getById($record->app);
         $buildModel->startTime = strtotime($record->startTime);
         $buildModel->endTime = strtotime($record->endTime);
 
--- core/services/BuildService.php
+++ core/services/BuildService.php
@@ -1,27 +1,31 @@
 <?php namespace Partham\core\services;
 
 use Partham\core\mappers\BuildModelMapper;
+use Partham\core\repositories\AppRepository;
 use Partham\core\repositories\BuildRepository;
 
 class BuildService
 {
     private $database;
     private $repository;
+    private $appRepository;
 
     public function __construct()
     {
         $this->database = new DatabaseService();
         $this->repository = new BuildRepository($this->database);
+        $this->appRepository = new AppRepository($this->database);
     }
 
     public function getAll()
     {
         $response = [];
+        $mapper = new BuildModelMapper($this->appRepository);
 
         $data = $this->repository->getAll();
 
         foreach ($data as $record)
-            $response[] = BuildModelMapper::map($record);
+            $response[] = $mapper->map($record);
 
         return $response;
     }
