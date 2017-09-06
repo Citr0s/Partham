09316e87aeb7fdeab22cde0e1d25a16efa895e01
Milosz Dura <milosz.md@wp.pl>
2017-09-06 21:00:21 +0100

    hiding element instead of showing empty string

--- web/js/Builds/BuildService.ts
+++ web/js/Builds/BuildService.ts
@@ -29,10 +29,12 @@ class BuildService {
         document.querySelector('.process-box').setAttribute('style', 'display:block;');
 
         if (!parsedData[0].startTime && !parsedData[0].endTime) {
-            document.querySelector('.process-box .build-time').innerHTML = '';
+            document.querySelector('.process-box .build-time').setAttribute('style', 'display:none;');
             document.querySelector('.process-box .message').innerHTML = `<span style="font-weight:bold;">${parsedData[0].appName}</span> is queued for build.`;
             return;
         }
+
+        document.querySelector('.process-box .build-time').setAttribute('style', 'display:block;');
 
         let secondsSinceStart = Math.floor((new Date().getTime()) / 1000 - parsedData[0].startTime);
         let minutesSinceStart = Math.floor(secondsSinceStart / 60);
