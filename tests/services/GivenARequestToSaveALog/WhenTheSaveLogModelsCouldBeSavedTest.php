7f05e4d5b268a3d6d32da1fc3891708e47f99bb6
Milosz Dura <milosz.md@wp.pl>
2017-09-11 21:41:20 +0100

    adding test around log service save method

--- tests/services/GivenARequestToSaveALog/WhenTheSaveLogModelsCouldBeSavedTest.php
+++ tests/services/GivenARequestToSaveALog/WhenTheSaveLogModelsCouldBeSavedTest.php
@@ -2,66 +2,20 @@
 
 use Partham\core\interfaces\ILogRepository;
 use Partham\core\services\LogService;
-use Partham\core\types\LogRecord;
 
-class LogRepositoryStub implements ILogRepository
+class WhenTheLogModelsCouldBeSavedTest extends PHPUnit\Framework\TestCase
 {
-
-    public function getAll(): array
-    {
-        $record = new LogRecord();
-        $record->app = "Partham";
-        $record->severity = "Error";
-        $record->message = "Something went wrong!";
-        $record->exception = "PHP Fatal Exception: Something went wrong!";
-        $record->loggedAt = "2017-09-11 20:56";
-
-        return [$record];
-    }
-
-    public function save($severity, $logDetails)
-    {
-        // TODO: Implement save() method.
-    }
-}
-
-class WhenTheLogModelsCouldBeRetrievedTest extends PHPUnit\Framework\TestCase
-{
-    private $result;
+    private $mock;
 
     public function setUp()
     {
-        $subject = new LogService(new LogRepositoryStub());
-        $this->result = $subject->getAll();
+        $this->mock = $this->createMock(ILogRepository::class);
+        $this->mock->expects($this->once())->method('save')->with('1', '2');
     }
 
-    public function testOneResultIsReturned()
+    public function testLogRepositoryGetsCalledWithCorrectlyMappedParameters()
     {
-        $this->assertEquals(sizeof($this->result), 1);
-    }
-
-    public function testAppNameIsMappedCorrectly()
-    {
-        $this->assertEquals($this->result[0]->appName, "Partham");
-    }
-
-    public function testSeverityIsMappedCorrectly()
-    {
-        $this->assertEquals($this->result[0]->severity, "Error");
-    }
-
-    public function testMessageIsMappedCorrectly()
-    {
-        $this->assertEquals($this->result[0]->message, "Something went wrong!");
-    }
-
-    public function testExceptionIsMappedCorrectly()
-    {
-        $this->assertEquals($this->result[0]->exception, "PHP Fatal Exception: Something went wrong!");
-    }
-
-    public function testLoggedAtIsMappedCorrectly()
-    {
-        $this->assertEquals($this->result[0]->loggedAt, "2017-09-11 20:56");
+        $subject = new LogService($this->mock);
+        $subject->invoke('1', '2');
     }
 }
